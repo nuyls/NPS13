412ae0d57a06d37712162cd8b61220c5b052f443
NpS <preeyanan.nps@gmail.com>
2024-01-02 15:27:13 +0700

    13.2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M])
+{
+    for (int i = 0; i < M + 2; ++i) {
+        cout << '-';
+    }
+    cout << endl;
+
+    for (int i = 0; i < N; ++i) {
+        cout << '|';  
+        for (int j = 0; j < M; ++j) {
+            cout << (image[i][j] ? '*' : ' ');
+        }
+        cout << '|' << endl; 
+    }
+
+    for (int i = 0; i < M + 2; ++i) {
+        cout << '-';
+    }
+    cout << endl;
+}
+
+void updateImage(bool image[][M], int brushSize, int x, int y)
+{
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            double distance = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if (distance <= brushSize - 1) {
+                image[i][j] = true;
+            }
+        }
+    }
+}
